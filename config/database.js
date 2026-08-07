5c79be6d575bfae2430e0bf925def6d50fd8739d
nawrin30 <nawrintarannum30@gmail.com>
2026-08-07 23:14:10 +0600

    Add index.html to root for GitHub Pages

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-// Establish connection to SQLite database file
+
 const dbPath = path.join(__dirname, '..', 'database.db');
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
